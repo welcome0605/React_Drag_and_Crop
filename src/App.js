a23458ce99c2c284df3e3bcab1e7a3da733c4a85
welcome0605 <yuri.fedorov0605@protonmail.com>
2019-07-09 06:45:55 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+// import PostSorting from './posts/PostSorting'
+import './App.css'
+
+import ImgDropAndCrop from './learn/ImgDropAndCrop'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <ImgDropAndCrop />
+      </div>
+    )
+  }
+}
+
+export default App
